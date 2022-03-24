c8dd7301866899faf2cc9ae26556b6841c4857c2
vianabar <viana@bu.edu>
2022-03-24 15:56:54 -0400

    correct json files

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import json 
 import os
 
-chassis_file = open('Eco1C1G1T1.UCF.json')
+chassis_name = 'Eco1C1G1T1'
+input_dir = os.getcwd() + '/input/' 
+
+
+chassis_file = open(input_dir + 'Eco1C1G1T1.UCF.json')
 
 data = json.load(chassis_file)
 
 for i in data['collection']:
-    print(i)
+    print(i)    

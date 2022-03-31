b0925896113921891e10befd2c8a73e8c733c61c
tiffaluk <tiffaluk@bu.edu>
2022-03-30 21:35:53 -0400

    Added GUI code

--- GUI.py
+++ GUI.py
@@ -1,34 +1,111 @@
 from tkinter import *
+from tkinter import ttk
+# from PIL import ImageTK, Image
+# pip install Pillow
+
+
 
 root = Tk()
 root.title("Genetic Circuit Design Automation")
+# root.iconbitmap('filepath/iconname.ico')
+root.geometry("1280x720")
 
-# Button function
-def myClick():
-	myLabel = Label(root, text="Hello " + e.get())
-	myLabel.pack()
+frame = LabelFrame(root, width = 800, height = 500, text="This is my frame..", padx=100, pady=5)
+frame.pack(expand=True, fill='both')
+frame.pack_propagate(0)
 
-# Creating an Entry Widget
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=1, column=1, columnspan=3, padx=10, pady=10)
-e.insert(0,"Enter your name: ")
-
-# # Creating a Label Widget
-# myLabel1 = Label(root, text="Hello World!")
-# myLabel2 = Label(root, text="This is line 2")
-
-# # Put onto screen
-# myLabel1.grid(row=0, column=0)
-# myLabel2.grid(row=1, column=1)
-
-# Creating a Button
-myButton = Button(root, text="Enter your name!", command=myClick)
-#other parameters for button: state=DISABLED, padx=50, pady=50, fg, bg
-myButton.pack()
+p_frame = LabelFrame(root, width = 200 , height = 200, text="Promoters:")
+p_frame.pack(side = LEFT)
+p_frame.pack_propagate(0)
 
 
+g_frame = LabelFrame(root, width = 200 , height = 200, text="UCF Gates:")
+g_frame.pack(side = LEFT)
+g_frame.pack_propagate(0)
+
+o_frame = LabelFrame(root, width = 200 , height = 200, text="Output Gates:")
+o_frame.pack(side = LEFT)
+o_frame.pack_propagate(0)
 
 
+def selected_p(event):
+	myLabel = Label(p_frame, text = combo1.get()).pack()
+
+def selected_g(event):
+	myLabel = Label(g_frame, text = combo2.get()).pack()
+
+def selected_o(event):
+	myLabel = Label(o_frame, text = combo3.get()).pack()
+
+def generate_circuit():
+	myLabel = Label(frame, text = "Circuit will show here").pack()
+	button_circuit['state'] = 'disabled'
+
+def reset():
+	if button_circuit['state'] == 'disabled':
+		button_circuit['state'] = 'normal'
+
+	# for widget in p_frame.winfo_children():
+ #       widget.destroy()
+
+
+promoters = [
+	"Promoter1", 
+	"Promoter2", 
+	"Promoter3", 
+	"Promoter4"
+]
+
+UCF_gates = [
+	"UCF_g1", 
+	"UCF_g2", 
+	"UCF_g3", 
+	"UCF_g4"
+]
+
+output_gates = [
+	"output_g1", 
+	"output_g2", 
+	"output_g3", 
+	"output_g4"
+]
+
+
+# clicked = StringVar()
+# clicked.set("Select Promoters")
+
+# drop = OptionMenu(root, clicked, *promoters)
+# drop.pack()
+
+combo1 = ttk.Combobox(root, value=promoters)
+combo1.set("Select Promoter(s)")
+combo1.bind("<<ComboboxSelected>>", selected_p)
+combo1['state'] = 'readonly'
+combo1.pack()
+
+combo2 = ttk.Combobox(root, value=UCF_gates)
+combo2.set("Select UCF gate(s)")
+combo2.bind("<<ComboboxSelected>>", selected_g)
+combo2['state'] = 'readonly'
+combo2.pack()
+
+combo3 = ttk.Combobox(root, value=UCF_gates)
+combo3.set("Select output gate(s)")
+combo3.bind("<<ComboboxSelected>>", selected_o)
+combo3['state'] = 'readonly'
+combo3.pack()
+
+button_reset = Button(root, text="Start over", command=reset)
+button_reset.pack(side=RIGHT)
+
+button_circuit = Button(root, text="Design circuit", command=generate_circuit)
+button_circuit.pack(side=RIGHT)
+
+button_quit = Button(root, text="Exit", command=root.quit)
+button_quit.pack(side= BOTTOM)
+
+#my_img = ImageTk.PhotoImage(Image.open("filename.png"))
+#my_label = Label(image=my_img)
+#my_label.pack()
 
 root.mainloop()
-
